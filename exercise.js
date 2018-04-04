c28ddb020abfc0f2dc3acbebeb67fb86c9fb53ec
basurounaq11 <basurounaq11@gmail.com>
2018-04-04 10:49:56 -0400

    Finished exercise

--- exercise.js
+++ exercise.js
@@ -12,13 +12,25 @@ areaRange  = areaMax-areaMin;
 exercise.measureDistances = function(rooms,area){
 	// ------------------------
 	// YOUR CODE
-	// ------------------------	
+	// ------------------------
+	data.forEach(function (residence){
+		var deltaRooms = residence.rooms - rooms;
+		deltaRooms = deltaRooms/roomsRange;
+
+		var deltaArea = residence.area - area;
+		deltaArea = deltaArea/areaRange;
+
+		residence.distance = Math.sqrt(Math.pow(deltaRooms, 2) + Math.pow(deltaArea, 2));
+	});	
 };
 
 exercise.sortByDistance = function () {
 	// ------------------------
 	// YOUR CODE
-	// ------------------------	
+	// ------------------------
+	data.sort(function (a,b){
+		return a.distance - b.distance;
+	});	
 };
 
 exercise.guessType = function(k){
@@ -31,6 +43,25 @@ exercise.guessType = function(k){
 		var guess = {type : "house", count : 2};
 		return guess;
 	*/
+
+	var types = {apartment: 0, house: 0, flat: 0};
+	var nn = data.slice(0,k);
+
+	nn.forEach(function(item){
+		if(item.type == 'apartment'){
+			types.apartment += 1;
+		} else if(item.type == 'house'){
+			types.house += 1;
+		} else if (item.type == 'flat'){
+			types.flat += 1;
+		}
+	});
+
+	var type = Object.keys(types).reduce(function(a,b){
+		return types[a] > types[b] ? a : b;
+	});
+
+	return {type: type, count: types[type]};
 };
 
 // share work
